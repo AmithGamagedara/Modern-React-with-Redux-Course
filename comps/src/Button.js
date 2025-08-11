d434824783ea2673a48294121f3e4437e43227e5
Amith Gamagedara <amithofficial00@gmail.com>
2025-08-11 21:36:21 +0530

    add: finalizing the variations

--- comps/src/Button.js
+++ comps/src/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import className from "classnames";
-
+import { twMerge } from "tailwind-merge";
 
 function Button({
   children,
@@ -13,19 +13,23 @@ function Button({
   outline,
   rounded,
 }) {
-
-  const classes = className('px-3 py-1.5 border', {
-    'border-blue-600 bg-blue-500 text-white': primary,
-    'border-gray-900 bg-gray-900 text-white': secondary,
-    'border-green-600 bg-green-500 text-white': success,
-    'border-yellow-600 bg-yellow-500 text-white': warning,
-    'border-red-600 bg-red-500 text-white': danger
-  })
-  return (
-    <button className={classes}>
-      {children}
-    </button>
+  const classes = twMerge(
+    className("px-3 py-1.5 border", {
+      "border-blue-600 bg-blue-500 text-white": primary, //primary is true
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-600 bg-green-500 text-white": success,
+      "border-yellow-600 bg-yellow-500 text-white": warning,
+      "border-red-600 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-500": outline && warning,
+      "text-red-500": outline && danger,
+    })
   );
+  return <button className={classes}>{children}</button>;
 }
 
 // Button.propTypes = {
@@ -47,4 +51,3 @@ function Button({
 // };
 
 export default Button;
- 
