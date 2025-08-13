fd71e65f4a08b28bd270ba349f4ef56eb3476ccf
Amith Gamagedara <amithofficial00@gmail.com>
2025-08-13 18:15:40 +0530

    add- update dropdown component

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function DropDown({options}) {
+function DropDown({ options, selection, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    //close dropdown
+    setIsOpen(false);
+    // what option did the user click on?
+    onSelect(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+
+  //   let content = 'Select...';
+  //   if(selection){
+  //     content = selection.label;
+  //   }
+
   return (
-    <div>DropDown</div>
-  )
+    <div>
+      <div onClick={handleClick}>{selection?.label || "Select..."}</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
-export default DropDown
+export default DropDown;
