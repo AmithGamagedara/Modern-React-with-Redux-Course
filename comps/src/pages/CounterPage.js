70120fe16adae89d240b62592a2fdcd3b2b5b72d
Amith Gamagedara <amithofficial00@gmail.com>
2025-08-20 14:01:09 +0530

    add: update the counter page using immer

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
+import { produce } from 'immer';
 
 const INCREMENT_COUNT = "increment";
 const SET_VALUE_TO_ADD = "change_value_to_add";
@@ -10,33 +11,43 @@ const ADD_VALUE_TO_COUNT = "add_value_to_count";
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count +1;
+      return;
+      // return {
+      //   ...state,
+      //   count: state.count + 1,
+      // };
 
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count = state.count -1;
+      return;
+      // return {
+      //   ...state,
+      //   count: state.count - 1,
+      // };
 
     case ADD_VALUE_TO_COUNT:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      };
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+      // return {
+      //   ...state,
+      //   count: state.count + state.valueToAdd,
+      //   valueToAdd: 0,
+      // };
 
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
+      state.valueToAdd = action.payload;
+      return;
+      // return {
+      //   ...state,
+      //   valueToAdd: action.payload,
+      // };
 
     default:
+      return;
       // throw new Error('unexpected action type: ' + action.type);
-      return state;
+      // return state;
   }
   // if (action.type === INCREMENT_COUNT) {
   //   return {
@@ -57,7 +68,7 @@ function CounterPage({ initialCount }) {
   // const [count, setCount] = useState(initialCount);
   // const [valueToAdd, setValueToAdd] = useState(0);
 
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
